37d22b76d4b77ddf7cbf0846b7b31779e2c61006
Richard Melchior <raustin.melchior@gmail.com>
2018-06-17 14:07:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,20 +6,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-
-    if req.path.match(/cart/)
-      if @@cart == []
-        resp.write("Your cart is empty.")
-      else
-        cart.each do |item|
-          resp.write("#{item}\n")
-        end
-    elsif req.path.match(/add/)
+    if req.path.match(/items/)
      item = req.params["item"]
 
      if @@items.include?(item)
        item_price = item.price
-       resp.write "The price is: #{item.price}"
+       resp.write "The price is: #{item_price}"
      else
        resp.write "We don't have that item"
      end
