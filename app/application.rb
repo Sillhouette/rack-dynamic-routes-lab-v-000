68b9d5f263a73fa6115a0224fb3fa87bfc28b259
Richard Melchior <raustin.melchior@gmail.com>
2018-06-17 14:14:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path == "/items"
        item = req.params["items"]
 
        if @@items.include?(item)
